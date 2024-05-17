80501b6c9319bac3f00be90bf6b2c8d2f9dc4492
ifs21034-itdel <ifs21034@students.del.ac.id>
2024-05-17 10:33:05 +0700

    add seeder tnggal

--- database/seeders/GenerateTanggalSeeder.php
+++ database/seeders/GenerateTanggalSeeder.php
@@ -17,22 +17,22 @@ class GenerateTanggalSeeder extends Seeder
         generate_tanggal::create([
             'tipe' => 'FED',
             'tgl_awal_pengisian' => '2024-06-01',
-            'tgl_akhir_pengisian' => '2024-06-10',
-            'periode_awal_approve_assesor_1' => '2024-06-11',
-            'periode_akhir_approve_assesor_1' => '2024-06-15',
-            'periode_awal_approve_assesor_2' => '2024-06-16',
-            'periode_akhir_approve_assesor_2' => '2024-06-20',
+            'tgl_akhir_pengisian' => '2025-06-30',
+            'periode_awal_approve_assesor_1' => '2024-06-01',
+            'periode_akhir_approve_assesor_1' => '2025-06-30',
+            'periode_awal_approve_assesor_2' => '2025-06-30',
+            'periode_akhir_approve_assesor_2' => '2025-06-30',
             'tahun_ajaran' => '2023/2024',
         ]);
 
         generate_tanggal::create([
             'tipe' => 'FRK',
             'tgl_awal_pengisian' => '2024-01-01',
-            'tgl_akhir_pengisian' => '2024-01-10',
-            'periode_awal_approve_assesor_1' => '2024-01-11',
-            'periode_akhir_approve_assesor_1' => '2024-01-15',
-            'periode_awal_approve_assesor_2' => '2024-01-16',
-            'periode_akhir_approve_assesor_2' => '2024-01-20',
+            'tgl_akhir_pengisian' => '2024-12-31',
+            'periode_awal_approve_assesor_1' => '2024-01-01',
+            'periode_akhir_approve_assesor_1' => '2024-12-31',
+            'periode_awal_approve_assesor_2' => '2024-01-01',
+            'periode_akhir_approve_assesor_2' => '2024-12-31',
             'tahun_ajaran' => '2023/2024',
         ]);
 
